7598bc4a8772ba4e3f0653113fc6dd2c249c7310
mark <notnotjudas@gmail.com>
2026-06-10 23:08:37 +0330

    updated exc

--- backend/src/common/exceptions.py
+++ backend/src/common/exceptions.py
@@ -7,16 +7,3 @@ class ApplicationException(Exception):
     def __init__(self, message=None):
         if message:
             self.message = message
-
-
-
-class OTPExpiredException(ApplicationException):
-
-    status_code = 400
-    message = "OTP has expired."
-    error_code = "OTP_EXPIRED"
-class OTPInvalidException(ApplicationException):
-
-    status_code = 400
-    message = "Invalid OTP."
-    error_code = "OTP_INVALID"
